67046fc1224254c7aa20231a2fd49e994ba10ff3
gabriel2443 <gabriel.iorga200@gmail.com>
2024-05-30 18:22:59 +0200

    Add project files.

---
+++ Phonebook/Models/ContactDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Phonebook.Models;
+
+public class ContactDetails
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+}
---
+++ Phonebook/Program.cs
@@ -0,0 +1,15 @@
+﻿using Phonebook.Data;
+using Phonebook.Models;
+
+var context = new PhonebookContext();
+
+var contactDetail = new ContactDetails
+{
+    Name = "asd",
+    Email = "aiauaua",
+    Phone = "hauhau"
+};
+
+context.Add(contactDetail);
+
+context.SaveChanges();
